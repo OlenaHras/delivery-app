845d88bae7706975ddfa70d789c29d881465cac2
OlenaHras <alena.hras@gmail.com>
2023-06-03 00:49:23 +0300

    added baseName

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/delivery">
+  <BrowserRouter basename="/delivery-app">
     <App />
   </BrowserRouter>
 );
