de208273f72dc72ae81249e13498680dda1cb3fb
OlenaHras <alena.hras@gmail.com>
2023-06-03 10:06:48 +0300

    cleaned public

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter
-  // basename="/delivery-app"
-  >
+  <BrowserRouter basename="/delivery-app">
     <App />
   </BrowserRouter>
 );
